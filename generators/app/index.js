c50b4d0908e26557ce536330ac37e6cd0a6535be
Emil Stenberg <emilsten@kth.se>
2017-10-23 13:16:44 +0200

    added options

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,46 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+'use strict'
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
 
 module.exports = class extends Generator {
-  prompting() {
+  prompting () {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the dandy ' + chalk.red('generator-kth-node-api') + ' generator!'
-    ));
+    ))
 
     const prompts = [{
       type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+      name: 'useMongo',
+      message: 'Would you like to use a Mongo database?',
+      default: false
+    },{
+      type: 'confirm',
+      name: 'useAuth',
+      message: 'Would you like to use authentication?',
+      default: false
+    },{
+      type: 'confirm',
+      name: 'useSwagger',
+      message: 'Would you like to use Swagger?',
+      default: false
+    }]
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.props = props
+    })
   }
 
-  writing() {
+  writing () {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
-    );
+    )
   }
 
-  install() {
-    this.installDependencies();
+  install () {
+    this.installDependencies()
   }
-};
+}
